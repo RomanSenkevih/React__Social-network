814f4449fe7403e8fffd771164fd73da5463e133
roman <gosahorosij328@gmail.com>
2023-04-03 11:04:40 +0300

    видео 21(верстка страницы диалогов)

--- Social-network/src/components/Dialogs/Dialogs.jsx
+++ Social-network/src/components/Dialogs/Dialogs.jsx
@@ -3,9 +3,41 @@ import s from './Dialogs.module.css'
 const Dialogs = (props) => {
     return (
         <>
-            <h2>
-            dialogues
-            </h2>
+            <div className={s.dialogs}>
+                <div className={s.messageAuthors}>
+                   <div className={s.author}>Vasa</div>
+                   <div className={s.author}>Kata</div>
+                   <div className={s.author}>Dima</div>
+                   <div className={s.author}>Lena</div>
+                   <div className={s.author}>Misha</div>
+                   <div className={s.author}>Andry</div>
+                   <div className={s.author}>Vova</div>
+                   <div className={s.author}>Sveta</div>
+                </div>
+                <div className={s.messages}>
+                   <div className={s.message}>Notice the use of %PUBLIC_URL% in the tags above.
+      It will be replaced with the URL of the `public` folder during the build.
+      Only files inside the `public` folder can be referenced from the HTML</div>
+                   <div className={s.message}>Notice the use of %PUBLIC_URL% in the tags above.
+      It will be replaced with the URL of the `public` folder during the build.
+      Only files inside the `public` folder can</div>
+                   <div className={s.message}>Notice the use of %PUBLIC_URL% in the tags above.
+      It will be replaced with the URL of the `public` folder during the build</div>
+                   <div className={s.message}>Notice the use of %PUBLIC_URL% in the tags above.
+      It will be replaced with the URL of the `public` folder during the build.
+      Only files inside the `public` folder can be referenced from the HTML</div>
+                   <div className={s.message}>Notice the use of %PUBLIC_URL% in the tags above.
+      It will be replaced with the URL of the `public` folder during the build.
+      Only files inside the `public` folder c</div>
+                   <div className={s.message}>Notice the use of %PUBLIC_URL% in the tags above.
+      It will be replaced with the URL of the `public`</div>
+                   <div className={s.message}>Notice the use of %PUBLIC_URL% in the tags above.
+      It will be replaced with the URL of the `public` folder during the build.
+      Only files inside the `pu</div>
+                   <div className={s.message}>Notice the use of %PUBLIC_URL% in the tags above.
+      It will be replaced with the URL of the `public`</div>
+                </div>
+            </div>
         </>
     )
 }
