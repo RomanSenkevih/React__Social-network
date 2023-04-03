b390ba069785f3e3c600e86dbdf889a44b600647
roman <gosahorosij328@gmail.com>
2023-04-03 11:41:33 +0300

    видео 22(route exact)

--- Social-network/src/components/Dialogs/Dialogs.jsx
+++ Social-network/src/components/Dialogs/Dialogs.jsx
@@ -1,18 +1,19 @@
-import s from './Dialogs.module.css'
+import s from './Dialogs.module.css';
+import { NavLink } from "react-router-dom";
 
 const Dialogs = (props) => {
     return (
         <>
             <div className={s.dialogs}>
                 <div className={s.messageAuthors}>
-                   <div className={s.author}>Vasa</div>
-                   <div className={s.author}>Kata</div>
-                   <div className={s.author}>Dima</div>
-                   <div className={s.author}>Lena</div>
-                   <div className={s.author}>Misha</div>
-                   <div className={s.author}>Andry</div>
-                   <div className={s.author}>Vova</div>
-                   <div className={s.author}>Sveta</div>
+                   <NavLink to="/dialogs/1" className={s.author}>Vasa</NavLink>
+                   <NavLink to="/dialogs/2" className={s.author}>Kata</NavLink>
+                   <NavLink to="/dialogs/3" className={s.author}>Dima</NavLink>
+                   <NavLink to="/dialogs/4" className={s.author}>Lena</NavLink>
+                   <NavLink to="/dialogs/5" className={s.author}>Misha</NavLink>
+                   <NavLink to="/dialogs/6" className={s.author}>Andry</NavLink>
+                   <NavLink to="/dialogs/7" className={s.author}>Vova</NavLink>
+                   <NavLink to="/dialogs/8" className={s.author}>Sveta</NavLink>
                 </div>
                 <div className={s.messages}>
                    <div className={s.message}>Notice the use of %PUBLIC_URL% in the tags above.
