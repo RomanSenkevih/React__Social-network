c618049e8bddb332f37699ffa1b264de110b9013
roman <gosahorosij328@gmail.com>
2023-04-04 15:58:10 +0300

    видео 23(props для Диалогов)

--- Social-network/src/components/Dialogs/Dialogs.jsx
+++ Social-network/src/components/Dialogs/Dialogs.jsx
@@ -1,42 +1,30 @@
 import s from './Dialogs.module.css';
 import { NavLink } from "react-router-dom";
 
+function MessageAuthors(props) {
+   return  <NavLink to={"/dialogs/" + props.thePathToTheFile} className={s.author}>{props.name}</NavLink>
+}
+
+function Messages(props) {
+    return <div className={s.message}>{props.message}</div>
+}
+
 const Dialogs = (props) => {
     return (
         <>
             <div className={s.dialogs}>
                 <div className={s.messageAuthors}>
-                   <NavLink to="/dialogs/1" className={s.author}>Vasa</NavLink>
-                   <NavLink to="/dialogs/2" className={s.author}>Kata</NavLink>
-                   <NavLink to="/dialogs/3" className={s.author}>Dima</NavLink>
-                   <NavLink to="/dialogs/4" className={s.author}>Lena</NavLink>
-                   <NavLink to="/dialogs/5" className={s.author}>Misha</NavLink>
-                   <NavLink to="/dialogs/6" className={s.author}>Andry</NavLink>
-                   <NavLink to="/dialogs/7" className={s.author}>Vova</NavLink>
-                   <NavLink to="/dialogs/8" className={s.author}>Sveta</NavLink>
+                    <MessageAuthors name="Vasa" thePathToTheFile="1" />
+                    <MessageAuthors name="Vika" thePathToTheFile="2" />
+                    <MessageAuthors name="Roma" thePathToTheFile="3" />
+                    <MessageAuthors name="Dima" thePathToTheFile="4" />
+                    <MessageAuthors name="Shura" thePathToTheFile="5" />
                 </div>
                 <div className={s.messages}>
-                   <div className={s.message}>Notice the use of %PUBLIC_URL% in the tags above.
-      It will be replaced with the URL of the `public` folder during the build.
-      Only files inside the `public` folder can be referenced from the HTML</div>
-                   <div className={s.message}>Notice the use of %PUBLIC_URL% in the tags above.
-      It will be replaced with the URL of the `public` folder during the build.
-      Only files inside the `public` folder can</div>
-                   <div className={s.message}>Notice the use of %PUBLIC_URL% in the tags above.
-      It will be replaced with the URL of the `public` folder during the build</div>
-                   <div className={s.message}>Notice the use of %PUBLIC_URL% in the tags above.
-      It will be replaced with the URL of the `public` folder during the build.
-      Only files inside the `public` folder can be referenced from the HTML</div>
-                   <div className={s.message}>Notice the use of %PUBLIC_URL% in the tags above.
-      It will be replaced with the URL of the `public` folder during the build.
-      Only files inside the `public` folder c</div>
-                   <div className={s.message}>Notice the use of %PUBLIC_URL% in the tags above.
-      It will be replaced with the URL of the `public`</div>
-                   <div className={s.message}>Notice the use of %PUBLIC_URL% in the tags above.
-      It will be replaced with the URL of the `public` folder during the build.
-      Only files inside the `pu</div>
-                   <div className={s.message}>Notice the use of %PUBLIC_URL% in the tags above.
-      It will be replaced with the URL of the `public`</div>
+                   <Messages message="Yo" />
+                   <Messages message="You ma dog" />
+                   <Messages message="hooy world" />
+                   <Messages message="red xxx" />
                 </div>
             </div>
         </>
--- Social-network/src/components/Profile/MyPosts/MyPosts.jsx
+++ Social-network/src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,19 +3,21 @@ import Post from './Post/Post';
 
 const MyPosts = () => {
     return (
-        <>
+        <div className={s.container}>
             <div>
                 <label for="xxx"><h2>My posts</h2></label>
                 <textarea id='xxx' className={s.input} ></textarea>
+                <div className={s.buttonLeft}>
                 <button>Add post</button>
                 <button>Remove</button>
+                </div>
             </div>
             <div>
                <Post messeg=' Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, odio! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere neque quas, reiciendis accusantium qui velit vel cumque deleniti modi eius.' like='197'/>
                <Post messeg='сообщение 2 привет мир' like='55'/>
                <Post messeg='сообщение 3  tetur adipisicing elit. Facere neque quas, reiciendis accusantium qui velit vel cumque deleniti modi eius.' like='88'/>
             </div>
-        </>
+        </div>
     )
 }
 
